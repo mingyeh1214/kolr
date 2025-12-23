8b5d568d18a7da8b3c54501aa7c5c44b9b8833eb
mingyeh1214 <mingyeh1214@gmail.com>
2025-12-23 17:28:18 +0800

    update

---
+++ kolrapp/test.tsx
@@ -0,0 +1,8 @@
+function sayHello(person: string) {
+    return 'Hello, ' + person + '!'
+}
+
+const user = 'Tom'
+console.log(sayHello(user))
+const createdByNewBoolean: Boolean = new Boolean(1)
+console.log(createdByNewBoolean)
